1c9cc1915ac45fa33a38ab275ce52479897ff25d
shunto <s1610360@edu.cc.uec.ac.jp>
2019-11-22 14:59:40 +0900

    :sparkles: ログイン前の画面作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="home">
-    <h1>This is a home page</h1>
+  <div class="container">
+    <div class="row">
+      <div class="top-message col-md-offset-3 col-md-6">
+        <h1>Welcome to Bookers!!</h1>
+        <p>In Bookers,</p>
+        <p>you can share and exchange  your opinions , inpressions , and emotions</p>
+        <p> about varioius books!</p>
+        <div class="btn-wrapper">
+          <div class="row">
+            <router-link to="signin" class="btn btn-info col">Log in</router-link>
+            <router-link to="signup" class="btn btn-info col">Sign up</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'home',
   components: {
   }
 }
 </script>
+
+<style>
+.btn-info {
+  margin: 0 10px;
+}
+</style>
