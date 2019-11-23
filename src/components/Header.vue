8a3701002af1085a896ca09884b4f611a661d7b3
shunto <s1610360@edu.cc.uec.ac.jp>
2019-11-23 10:40:29 +0900

    :recycle: コンポーネントに分割

---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <header class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <nav>
+        <span class="user-name">{{ user.email }}</span>
+      </nav>
+      <nav v-if="user.uid" key="login">
+        <ul>
+          <li class="nav-item"><router-link to="/users" class="nav-link">User</router-link></li>
+          <li class="nav-item"><router-link to="/books" class="nav-link">Book</router-link></li>
+          <li class="nav-item"><a @click="doLogout" class="nav-link logout" style="color:#007bff;">Logout</a></li>
+        </ul>
+      </nav>
+      <nav v-else key="logout">
+        <ul>
+          <li class="nav-item"><router-link to="/" class="nav-link">Home</router-link></li>
+          <li class="nav-item"><router-link to="/about" class="nav-link">About</router-link></li>
+          <li class="nav-item"><router-link to="/signin" class="nav-link">Login</router-link></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+  import firebase from '@/plugin/firebase';
+  export default {
+    data() {
+      return {
+        user: {},
+      }
+    },
+    created() {
+      firebase.auth().onAuthStateChanged(user => {
+          this.user = user ? user : {}
+      })
+    },
+    methods: {
+      doLogout() {
+        firebase.auth().signOut()
+      },
+    }
+  }
+</script>
+
+<style>
+  nav > ul {
+    list-style: none;
+  }
+  nav > ul > li {
+    display: inline-block;
+  }
+  .user-name {
+    color: white;
+  }
+  .logout {
+    cursor: pointer;
+  }
+  .router-link-exact-active {
+    color: deeppink;
+  }
+</style>
